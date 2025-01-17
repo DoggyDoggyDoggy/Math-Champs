d6fce8a81cab4f82be7088f20ae8a42fe8609078
Doggy <pedan.denys@gmail.com>
2025-01-18 03:57:26 +1300

    Added fonts. Edit Home page. Change color if user answer wrong or right. Moved EquationAnswersCreator and EquationCreator to repository folder

--- app/src/main/java/denys/diomaxius/mathchamps/ui/game/Game.kt
+++ app/src/main/java/denys/diomaxius/mathchamps/ui/game/Game.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
